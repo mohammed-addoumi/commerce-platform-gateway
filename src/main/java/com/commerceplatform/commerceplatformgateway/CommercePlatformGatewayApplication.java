53108559d4c61fea9f9d7b6c29181e91d22fdae2
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-05-22 14:32:21 +0100

    feat: init project

---
+++ src/main/java/com/commerceplatform/commerceplatformgateway/CommercePlatformGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.commerceplatform.commerceplatformgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePlatformGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercePlatformGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerceplatform/commerceplatformgateway/CommercePlatformGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerceplatform.commerceplatformgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePlatformGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
